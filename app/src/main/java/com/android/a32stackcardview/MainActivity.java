d6bac753bb38383022966628af603bb97cf4131f
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-05-03 05:45:59 +0700

    Initial commit

---
+++ app/src/main/java/com/android/a32stackcardview/MainActivity.java
@@ -0,0 +1,14 @@
+package com.android.a32stackcardview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
